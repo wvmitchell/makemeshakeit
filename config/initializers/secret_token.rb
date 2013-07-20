57d1801e1e43de47d28061d2b07c6affca6d220e
William Mitchell <wvmitchell@gmail.com>
2013-07-20 12:55:17 -0600

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Makemeshakeit::Application.config.secret_token = 'ff11da4b48c878eafb34958d07dbb9c83b857464cc0f6332ecddefe7e935a4e7074ffd8076482f93a738d801e76da18ea294c87e313d7ee497c367d1fc3e5634'
